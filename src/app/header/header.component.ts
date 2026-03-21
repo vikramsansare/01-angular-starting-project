eeb1879e9a91d1529c8b9c76138e4231dc6cfc91
Vikram Sansare <vikramsansare@gmail.com>
2026-03-21 19:15:21 +0530

    initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  styleUrl: './header.component.css',
+  templateUrl: './header.component.html',
+  standalone: true
+})
+
+export class HeaderComponent {
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,40 @@
+import { Component, computed, Input, signal, input, Output, EventEmitter, output } from '@angular/core';
+import { User } from './user.model';
+
+// type User = {
+//   id: string,
+//   avatar: string,
+//   name: string
+// }
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input() selected?: boolean;
+
+  // @Input({required:true}) avatar!:string;
+  // @Input({required:true}) name!:string;
+  // @Input({required: true}) id!:string;
+  @Output() select = new EventEmitter<string>();
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // id = input.required<string>();
+  // select = output<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    // const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.user.id);
+  }
+}
